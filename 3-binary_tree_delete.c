e6128cfb0ed3797b2b81f9da7c838b4462f78dd4
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-23 11:35:16 -0800

    binary trees task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,9 +8,8 @@ void binary_tree_delete(binary_tree_t *tree)
 		return;
 	}
 
-	free(tree->left);
-	free(tree->right);
-	free(tree->parent);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);:zx
+
 	free(tree);
-	return;
 }
