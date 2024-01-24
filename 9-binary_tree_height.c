ff18b2e5a1e32bd640f80d81274420ecb9b642da
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-24 08:46:45 -0800

    Binary trees task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,37 +8,25 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t height;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	left_height = 0;
-	right_height = 0;
+	height = 0;
 
 	if (tree->left != NULL)
 	{
-		left_height++;
+		height++;
 		binary_tree_height(tree->left);
 	}
 
 	if (tree->right != NULL)
 	{
-		right_height++;
+		height++;
 		binary_tree_height(tree->right);
 	}
-
-	if (left_height > right_height)
-		return (left_height);
-
-	if (left_height < right_height)
-		return (right_height);
-
-	if (left_height == right_height)
-	{
-		return (left_height + right_height);
-	}
-	return (0);
+	return (height);
 }
