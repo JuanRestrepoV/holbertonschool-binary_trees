d06e797ac70677de1d45a9f21f64c19c1b0094e7
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-01-26 08:00:10 -0800

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,6 +13,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
 	if (tree->left != NULL && tree->right !=NULL && tree->left->left == NULL && tree->right->right == NULL)
 	{
 		return (1);
